23d2e82872adfe0667d9b4fcae559c32172452ef
Joe Acosta <furryjoebear@gmail.com>
2024-10-28 12:30:40 -0700

    clean up image stuff

--- js/messier-min.js
+++ js/messier-min.js
@@ -137,13 +137,14 @@ window.addEventListener('DOMContentLoaded', () => {
                         <br>Date: ${objectData.date}</div>`;
                 }
                 image.innerHTML = `${results}<img src="/images/messier/${dset.image}" width="800px" height="600px">`;
-                const img = image.getElementsByTagName('img')[0];
-                const imgObj = getComputedStyle(img);
-                const iWidth = imgObj.getProperyValue('width');
-                const iHeight = imgObj.getProperyValue('height');
+                const imgiObj = image.getElementsByTagName('img')[0];
+                const imgStyleObj = getComputedStyle(imgObj);
+                const iWidth = imgStyleObj.getProperyValue('width');
+                const iHeight = imgStyleObj.getProperyValue('height');
                 if (iWidth && iHeight) {
                     const iWidthVal = parseInt(iWidth);
                     const iHeightVal = parseInt(iHeight);
+                    const ratio = Math.floor(imgWidthVal * 100 / 800) * 100;
                 }
             } else {
                 image.innerHTML = '';
