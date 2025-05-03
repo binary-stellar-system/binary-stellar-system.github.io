0ee0171acbe25ff59a5d121ba0a5370de3868390
josepha48 <josepha48@gmail.com>
2025-05-03 12:52:26 -0700

    handler updates

--- js/playsound.js
+++ js/playsound.js
@@ -1,17 +1,26 @@
 
 
+const getSelectValue(selectObj) {
+
+    if(!selectObj) {
+        return undefined;
+    }
+    console.log(selectObj.selectedIndex);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 
-    // handles to elements
-    const note = document.getElementById('sound-note');
-    const sharpFlat = document.getElementById('sound-sharp-flat');
-    const octave = document.getElementById('sound-octave');
-    const waveforme = document.getElementById('sound-waveform');
-    const duration = document.getElementById('sound-duration');
-
     document.addEventListener('click', event => {
-        const target = event.target;
-    
-        console.log(target);
+        const target = event?.target?.id;
+        if (target === 'form-submit') {
+            // handles to elements
+            const note = document.getElementById('sound-note');
+            const sharpFlat = document.getElementById('sound-sharp-flat');
+            const octave = document.getElementById('sound-octave');
+            const waveforme = document.getElementById('sound-waveform');
+            const duration = document.getElementById('sound-duration');
+            
+            console.log(getSelectValue(note));
+        }
     });
 });
