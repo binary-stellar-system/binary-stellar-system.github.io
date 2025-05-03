c3cb1988032229de13579daf4adb3c617791061d
josepha48 <josepha48@gmail.com>
2025-05-03 12:41:07 -0700

    add ids to elements and update js code

--- js/playsound.js
+++ js/playsound.js
@@ -2,8 +2,16 @@
 
 document.addEventListener('onDomContentLoaded', () => {
 
+    // handles to elements
+    const note = document.getElementById('sound-note');
+    const sharpFlat = document.getElementById('sound-sharp-flat');
+    const octave = document.getElementById('sound-octave');
+    const waveforme = document.getElementById('sound-waveform');
+    const duration = document.getElementById('sound-duration');
+
     document.addEventListener('click', event => {
         const target = event.target;
-        // 
+    
+        console.log(target);
     }
 });
